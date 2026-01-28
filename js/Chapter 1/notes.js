8069c8a7b52d28ba2e83f3937a7e75be72067606
ShubhamMehrotra <shubhammehrotra851@gmail.com>
2026-01-28 23:05:27 +0530

    main

---
+++ js/Chapter 1/notes.js
@@ -0,0 +1,38 @@
+// IT USES REPL MEANS READ EVAL PRINT LOOP   
+// variable is the name of the storage location ( they are basically container to store data values )
+// examples:- a=10; b="hello world"; c=true;
+// var, let, const are the three ways to declare a variable in javascript
+/*  we basically dnt use var  we use con for values which will not change and let which will get changed */
+
+/* DATA TYPES :-
+   PRIMITIVE DATA TYPES 
+   NUMBER, BOOLEAN , NUM , SYMBOL , STRING, UNDEFINED, BIGHT
+     TYPE OF IS A  OPERAATOR WHICH TELLS WHICH TYPE OF DATA TYPE IS IT */
+a = 10;
+typeof a; // it will return number
+
+/* operations 
+a+b  etc */
+a=10;
+b=20;
+sum=a+b
+// it will give 30
+// there are similar operations for subtraction, multiplication , division 
+// but there are two new one modulo (reamainder operator) and exponentiation(power operator)
+10 % 3 // it will give 1 as remainder(modulo operator)
+2 ** 3 // it will give 8 as 2 power 3(power operator)
+
+
+// NAN means not a number IN JS IT IS PROPERTY OF NUMBER TYPE
+/* EXAMPLES ARE 0/0 , NaN - 1,NaN * 1, NAN + NAN */
+
+// OPERATOR pRECEDENCE
+// NOTHING JUSTBODMAS RULE IT MEANS BRACKET OF DIVISION MULTIPLICATION ADDITION SUBTRACTION
+// IF SAME PRECEDENCE THEN LEFT TO RIGHT EVALUATION
+
+//  boolean operators
+    let age = 12;
+     let isadult = true ;
+     
+      
+
