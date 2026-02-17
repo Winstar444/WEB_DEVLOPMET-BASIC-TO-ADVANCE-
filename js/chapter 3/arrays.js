e36e253b1dbc9c35de90dc94e4cd61d5d484c4af
ShubhamMehrotra <shubhammehrotra851@gmail.com>
2026-02-18 02:25:50 +0530

    MAIN

---
+++ js/chapter 3/arrays.js
@@ -0,0 +1,6 @@
+let student = ["shuham", " hdnhe" , "ndjcc"]; 
+/* hoe to store arrays */
+
+//   ok if we want to know the index number (address ) of  a element  we write the name of variable and inside that barckets the index number 
+//  if you  put num[4] the value at  4  will gets printed.
+
